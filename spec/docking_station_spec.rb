5b5b352c65125b219b16b1451508acb340af8405
Imraan Ali <imraan91@hotmail.co.uk>
2020-05-29 21:10:10 +0100

    raising Exceptions

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,11 +8,6 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)). to eq bike
@@ -22,6 +17,20 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike). to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when docking station to release a bike when empty' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
 
