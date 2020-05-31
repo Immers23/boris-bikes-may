3b16c2f21e165c3c37ae78e50ae10f3d36d3b49f
Imraan Ali <imraan91@hotmail.co.uk>
2020-05-31 14:06:15 +0100

    using constants and namespace operator to access contants in rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -34,7 +34,7 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it 'raises an error when the docking station is full with 20 bikes' do
-      20.times { subject.dock Bike.new }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
