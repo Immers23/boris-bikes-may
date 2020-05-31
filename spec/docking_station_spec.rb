cb4dbef51b4fac2c58d75b5d47898e577dd29bc5
Imraan Ali <imraan91@hotmail.co.uk>
2020-05-31 10:18:14 +0100

    wrapping collections and using arrays to store objects

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,17 +4,17 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to :bikes }
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)). to eq bike
+    expect(subject.dock(bike)). to eq [bike]
   end
 
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike). to eq bike
+    expect(subject.bikes). to eq [bike]
   end
 
   describe '#release_bike' do
@@ -23,9 +23,7 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
-  end
 
-  describe '#release_bike' do
     it 'raises an error when docking station to release a bike when empty' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
@@ -35,9 +33,9 @@ describe DockingStation do
 
     it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it 'raises an error when full' do
-      subject.dock(Bike.new)
-      expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
+    it 'raises an error when the docking station is full with 20 bikes' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 
